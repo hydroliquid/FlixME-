da93f5b0493408c88e7af4e20c9a54bb2cc58da9
Michael Benton <hydromac@mikes-mac.local>
2019-02-01 11:03:32 -0800

    Initial Commit

---
+++ FlixME!Tests/FlixME_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  FlixME_Tests.swift
+//  FlixME!Tests
+//
+//  Created by Mike Benton on 2/1/19.
+//  Copyright © 2019 Mike Benton. All rights reserved.
+//
+
+import XCTest
+@testable import FlixME_
+
+class FlixME_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
